04d82eceb0ce65dc4e325baf01a982152e413f2f
Tazi Aymane <taziaymane68@gmail.com>
2025-12-23 22:59:42 +0100

    initial commit

---
+++ app/index.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { Text, View, StyleSheet } from 'react-native'
+
+export class home extends Component {
+  render() {
+    return (
+      <View>
+        <Text> textInComponent </Text>
+      </View>
+    )
+  }
+}
+
+export default home
+const style = StyleSheet.create({})
